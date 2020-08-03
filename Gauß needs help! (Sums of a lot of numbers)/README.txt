e2f789ea0dd9465a7dd87461fe2a340e6e247300
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2020-08-03 02:40:37 +0200

    7 kyu

---
+++ Gauß needs help! (Sums of a lot of numbers)/README.txt
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/54df2067ecaa226eca000229
---
+++ Gauß needs help! (Sums of a lot of numbers)/index.js
@@ -0,0 +1,6 @@
+function f(n) {
+    if (Number.isInteger(n) && n > 0) {
+        return (n * (n + 1)) / 2;
+    }
+    return false;
+};
