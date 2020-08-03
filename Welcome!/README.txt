50f57d062ddd50bb0a9d21d93a18e6ef48cff9cf
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2020-08-03 03:27:42 +0200

    8 kyu

---
+++ Welcome!/README.txt
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/577ff15ad648a14b780000e7
---
+++ Welcome!/index.js
@@ -0,0 +1,21 @@
+const database = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+
+const greet = language => database[language] === undefined ? database.english : database[language];
