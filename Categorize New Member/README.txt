a3247cecb9b0ea698ff6a34494e49a15020560ca
dawidzebacki <64470068+dawidzebacki@users.noreply.github.com>
2020-08-03 03:13:21 +0200

    7 kyu

---
+++ Categorize New Member/README.txt
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa
---
+++ Categorize New Member/index.js
@@ -0,0 +1,6 @@
+function openOrSenior(data) {
+    return data.map(element => {
+        if (element[0] >= 55 && element[1] > 7) return "Senior";
+        else return "Open";
+    });
+}
